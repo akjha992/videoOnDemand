11ad7303dbf218e0ae470cbb74382022c76a0dfb
Ashish <--global>
2019-04-24 21:01:51 +0530

    added new flash

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,6 @@
 export const data = [
     {
-        name: 'GOT S08E02 HD',
-        url: 'https://nl28.seedr.cc/ff_get/448373161/game.of.thrones.s08e02.720p.web.h264-memento[ettv].mkv?st=jKjvCyEcxp2gB_YmqldbPw&e=1556011743'
-    },
-    {
-        name: 'Big Bang Theory S12E09',
-        url: 'https://fr10.seedr.cc/ff_get/447327452/the.big.bang.theory.s12e19.hdtv.x264-lucidtv[ettv].mkv?st=SxHRbFHyEpQCA760cYEgyA&e=1555799512'
-    },
-    {
-        name: 'Flash S05E18',
-        url: 'https://nl17.seedr.cc/ff_get/447318420/the.flash.2014.s05e18.hdtv.x264-lucidtv[ettv].mkv?st=W1kyxjDQ1N4eiaJqQnC_1A&e=1555801057'
+        name: 'Flash S05E19',
+        url: 'https://fr3.seedr.cc/ff_get/449439982/the.flash.2014.s05e19.hdtv.x264-lucidtv[ettv].mkv?st=dlGDMtg_CIjLkWRVl_kNrg&e=1556206258'
     },
 ];
